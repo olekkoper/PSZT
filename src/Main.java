2d7689dd3818b151d293ba929f283623f52df14f
Michał Łępicki <michal.lepicki@gmail.com>
2016-01-16 21:04:10 +0100

    indent

--- src/Main.java
+++ src/Main.java
@@ -10,48 +10,46 @@ import org.neuroph.util.TransferFunctionType;
 */
 public class Main {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-// create training set (logical XOR function)
-DataSet trainingSet = new DataSet(2, 1);
-trainingSet.addRow(new DataSetRow(new double[]{0, 0}, new double[]{0}));
-trainingSet.addRow(new DataSetRow(new double[]{0, 1}, new double[]{1}));
-trainingSet.addRow(new DataSetRow(new double[]{1, 0}, new double[]{1}));
-trainingSet.addRow(new DataSetRow(new double[]{1, 1}, new double[]{0}));
+        // create training set (logical XOR function)
+        DataSet trainingSet = new DataSet(2, 1);
+        trainingSet.addRow(new DataSetRow(new double[]{0, 0}, new double[]{0}));
+        trainingSet.addRow(new DataSetRow(new double[]{0, 1}, new double[]{1}));
+        trainingSet.addRow(new DataSetRow(new double[]{1, 0}, new double[]{1}));
+        trainingSet.addRow(new DataSetRow(new double[]{1, 1}, new double[]{0}));
 
-// create multi layer perceptron
-MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 3, 1);
-// learn the training set
-myMlPerceptron.learn(trainingSet);
+        // create multi layer perceptron
+        MultiLayerPerceptron myMlPerceptron = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 3, 1);
+        // learn the training set
+        myMlPerceptron.learn(trainingSet);
 
-// test perceptron
-System.out.println("Testing trained neural network");
-testNeuralNetwork(myMlPerceptron, trainingSet);
+        // test perceptron
+        System.out.println("Testing trained neural network");
+        testNeuralNetwork(myMlPerceptron, trainingSet);
 
-// save trained neural network
-myMlPerceptron.save("myMlPerceptron.nnet");
+        // save trained neural network
+        myMlPerceptron.save("myMlPerceptron.nnet");
 
-// load saved neural network
-NeuralNetwork loadedMlPerceptron = NeuralNetwork.createFromFile("myMlPerceptron.nnet");
+        // load saved neural network
+        NeuralNetwork loadedMlPerceptron = NeuralNetwork.createFromFile("myMlPerceptron.nnet");
 
-// test loaded neural network
-System.out.println("Testing loaded neural network");
-testNeuralNetwork(loadedMlPerceptron, trainingSet);
+        // test loaded neural network
+        System.out.println("Testing loaded neural network");
+        testNeuralNetwork(loadedMlPerceptron, trainingSet);
 
-}
+    }
 
-public static void testNeuralNetwork(NeuralNetwork nnet, DataSet testSet) {
+    public static void testNeuralNetwork(NeuralNetwork nnet, DataSet testSet) {
 
-for(DataSetRow dataRow : testSet.getRows()) {
-nnet.setInput(dataRow.getInput());
-nnet.calculate();
-double[ ] networkOutput = nnet.getOutput();
-System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
-System.out.println(" Output: " + Arrays.toString(networkOutput) );
-}
+        for(DataSetRow dataRow : testSet.getRows()) {
+            nnet.setInput(dataRow.getInput());
+            nnet.calculate();
+            double[ ] networkOutput = nnet.getOutput();
+            System.out.print("Input: " + Arrays.toString(dataRow.getInput()) );
+            System.out.println(" Output: " + Arrays.toString(networkOutput) );
+        }
 
-}
-
- 
+    }
 
 }
